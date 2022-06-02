6dbaeb582a8095361305eaecaeacb68945e0b042
gabrieldacosta <gdc.business.2019@gmail.com>
2022-06-02 17:22:48 +0200

    feat(sign up) : adding form with django.contrib.auth.forms

--- litreview_project/authentication/views.py
+++ litreview_project/authentication/views.py
@@ -1,9 +1,27 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.contrib.auth.forms import UserCreationForm
+
 
 def auth_homepage(request):
     """View for home page / authentication"""
-    return render(request,'authentication/auth_homepage.html')
+    return render(request, 'authentication/auth_homepage.html')
+
 
 def sign_up_form(request):
     """View for signing up with filling a form"""
-    return render(request,'authentication/sign_up_form.html')
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            return HttpResponse(f"""<h1>Nouvel utilisateur créé avec succès !!</h1>
+            <p>Username : {username}</p>
+            <p>Password : {raw_password}""")
+        else:
+            return HttpResponse('<h1>form not valid</h1>')
+
+    else:
+        form = UserCreationForm()
+        return render(request, 'authentication/sign_up_form.html',{'form': form})
