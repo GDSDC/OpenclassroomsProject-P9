95a11d016ecabdacdefff5f6b090abe23d54b945
GDSDC <gdc.business.2019@gmail.com>
2022-06-07 17:42:31 +0200

    feature(custom forms) : creating custom forms for sign in and up

---
+++ litreview_project/authentication/forms.py
@@ -0,0 +1,19 @@
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django import forms
+
+
+class CustomUserCreationForm(UserCreationForm):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['username'].widget.attrs.update({'placeholder': 'Nom d\'utilisateur'})
+        self.fields['password1'].widget.attrs.update({'placeholder': 'Mot de passe'})
+        self.fields['password2'].widget.attrs.update({'placeholder': 'Confirmer votre mot de passe'})
+
+
+class CustomAuthenticationForm(AuthenticationForm):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['username'].widget.attrs.update({'placeholder': 'Nom d\'utilisateur'})
+        self.fields['password'].widget.attrs.update({'placeholder': 'Mot de passe'})
