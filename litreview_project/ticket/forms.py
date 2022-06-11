c62f70dd5d2eb13cc8259ac2f27de9150a3188e7
gabrieldacosta <gdc.business.2019@gmail.com>
2022-06-11 18:33:21 +0200

    feat(create ticket) : updating form view and template

--- litreview_project/ticket/forms.py
+++ litreview_project/ticket/forms.py
@@ -2,5 +2,7 @@ from django import forms
 
 class TicketForm(forms.Form):
     """Form for creating tickets"""
-    title = forms.CharField()
-    description = forms.CharField()
+    title = forms.CharField(max_length=128)
+    description = forms.CharField(max_length=2048)
+    image = forms.ImageField()
+    time_created = forms.DateTimeField()
--- litreview_project/ticket/views.py
+++ litreview_project/ticket/views.py
@@ -1,8 +1,18 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 from .forms import TicketForm
+from .models import Ticket
 
 
 def create_ticket(request):
     """View to create a ticket"""
-    form = TicketForm()
-    return render(request, 'ticket/create_ticket.html', {'form': form})
+    if request.method == 'POST':
+        title = request.POST.get('title', False)
+        description = request.POST.get('description', False)
+        image = request.POST.get('image', False)
+        ticket = Ticket(title=title, description=description, image=image)
+        ticket.save()
+        return HttpResponse(f"<h1>Votre ticket \"{title}\" a bien été enregistré !!</h1>")
+    else:
+        form = TicketForm()
+        return render(request, 'ticket/create_ticket.html', {'form': form})
