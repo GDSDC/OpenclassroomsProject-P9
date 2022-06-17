af9511c98d2a469a20b305b4efbdb87d36cd5882
GDSDC <gdc.business.2019@gmail.com>
2022-06-17 15:54:33 +0200

    feat(posts) : adding review posts in the posts page

--- litreview_project/posts/views.py
+++ litreview_project/posts/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render, redirect
 from ticket.models import Ticket
+from review.models import Review
 from django.views import View
 from django.contrib.auth.decorators import login_required
 from django.utils.decorators import method_decorator
+
 
 class posts_page(View):
     """View to see and change your own publications (ticket and/or reviews)"""
@@ -10,5 +12,8 @@ class posts_page(View):
     @method_decorator(login_required(login_url='/auth/'))
     def get(self, request):
         actual_user = request.user
-        tickets = Ticket.objects.filter(user=actual_user)
-        return render(request, 'posts/posts.html', {'tickets': tickets})
+        tickets = list(Ticket.objects.filter(user=actual_user))
+        reviews = list(Review.objects.filter(user=actual_user))
+        posts = [{'model': post.__class__.__name__, 'object': post} for post in tickets + reviews]
+
+        return render(request, 'posts/posts.html', {'posts': posts, 'rating_range': list(range(5))})
