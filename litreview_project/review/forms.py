3e00965a11f7ec61046a81bdb2d89aba1f7f344a
GDSDC <gdc.business.2019@gmail.com>
2022-06-28 18:05:38 +0200

    style

--- litreview_project/review/forms.py
+++ litreview_project/review/forms.py
@@ -9,6 +9,6 @@ class ReviewForm(forms.ModelForm):
         model = Review
         fields = ['ticket', 'rating', 'headline', 'body', 'user']
         widgets = {
-            'rating': forms.RadioSelect(
+            'rating': forms.RadioSelect( attrs={'class': 'row__display'},
                 choices=[(0, '- 0'), (1, '- 1'), (2, '- 2'), (3, '- 3'), (4, '- 4'), (5, '- 5')]),
         }
